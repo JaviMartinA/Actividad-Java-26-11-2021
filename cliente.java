daea8fc68ce24b16159d850059ea196c0fb61b3c
Javier Martin Arroyo <93380199+JaviMartinA@users.noreply.github.com>
2021-11-26 12:56:37 +0100

    Create cliente.java

---
+++ cliente.java
@@ -0,0 +1,32 @@
+public class Cliente{
+    private String nombre;
+    private String apellido;
+    private String ciudad;
+    private String empresa;
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setApellido(String apellido){
+        this.apellido=apellido;
+    }
+    public String getApellido(){
+        return this.apellido;
+    }
+    public void setCiudad(String ciudad){
+        this.ciudad=ciudad;
+    }
+    public String getCiudad(){
+        return this.ciudad;
+    }
+    
+    public void setEmpresa(String empresa){
+        this.empresa=empresa;
+    }
+    public String getEmpresa(){
+        return this.empresa;
+    }
+}
+    
